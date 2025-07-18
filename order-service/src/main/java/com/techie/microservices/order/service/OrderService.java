2e4f1f938b4593a0fff07b617bdac1ba0d923c2d
harikrishna t <harikrishna.tekkam@gmail.com>
2025-07-18 10:46:22 +0530

    added code files related to wiremock testing

--- order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
@@ -13,15 +14,22 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     public void placeOrder(OrderRequest orderRequest){
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock  = inventoryClient.inStock(orderRequest.skuCode(), orderRequest.quantity() );
 
-        orderRepository.save(order);
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
 
+            orderRepository.save(order);
+        }
+
+        else
+            throw new RuntimeException("Product with the skuCode "+orderRequest.skuCode()+" is not in the stock");
     }
 }
